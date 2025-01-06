1fb45eb9060f4933994b80f0eaa4d8e2d46d34b8
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2025-01-06 11:02:34 +0100

    Add project files.

---
+++ AmazingVilla_API/Controllers/VillaApiController.cs
@@ -0,0 +1,23 @@
+﻿using AmazingVilla_API.Dtos;
+using AmazingVilla_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmazingVilla_API.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaApi")]
+    [ApiController]
+    public class VillaApiController : ControllerBase 
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return new List<VillaDto>
+            {
+                new VillaDto {Id = 1, Name = "BarBeach" },
+                new VillaDto {Id = 2, Name = "ElegusiBeach" },
+                new VillaDto {Id = 3, Name = "ElekoBeach" }
+            };
+        }
+    }
+}
---
+++ AmazingVilla_API/Dtos/VillaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AmazingVilla_API.Dtos
+{
+    public class VillaDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
