edf62cd61ec0f5c4df30fec71217746b4d31d46a
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2025-01-10 11:02:53 +0100

    Configured EFC in AppDbContext.

--- AmazingVilla_API/Data/AppDbContext.cs
+++ AmazingVilla_API/Data/AppDbContext.cs
@@ -5,6 +5,10 @@ namespace AmazingVilla_API.Data
 {
     public class AppDbContext : DbContext 
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
         public DbSet<Villa> Villas { get; set; }
     }
 }
